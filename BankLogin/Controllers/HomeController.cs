5eb55e6bd1a0214daab0cbe43844f8230c608b8f
Vijayaraghavan R <rvrblr@gmail.com>
2024-01-19 13:34:38 +0530

    initial commit

---
+++ BankLogin/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using BankLogin.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BankLogin.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string username, string password)
+        {
+            
+            if (IsValidUser(username, password))
+            {
+                return RedirectToAction("Dashboard");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Invalid username or password";
+                return View();
+            }
+        }
+
+
+        private bool IsValidUser(string username, string password)
+        {
+            string defaultUsername = "username";
+            string defaultPassword = "password";
+
+            return username == defaultUsername && password == defaultPassword;
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Dashboard()
+        {
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ BankLogin/Models/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BankLogin.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Please enter a username.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Please enter a password.")]
+        public string Password { get; set; }
+    }
+}
